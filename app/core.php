1c9ae7035fdb05f241c4d1896e3b977fdadbe852
Sebastian Palmqvist <mystichal@live.se>
2023-11-20 20:11:26 +0100

    remove the moved files

--- app/core.php
+++
@@ -1,8 +0,0 @@
-<?php
-	namespace app;
-
-	class core {
-		public function run($router) {
-			$router->resolve();
-		}
-	}
--- models/home.php
+++
@@ -1,7 +0,0 @@
-<?php
-	namespace models;
-
-	use app\helpers;
-
-	class home {
-	}
